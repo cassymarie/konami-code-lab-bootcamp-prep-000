65b36f8d65ef8d07d570ceec60d9d4a74f5b6545
cassandraortiz <cassy.ortiz5@gmail.com>
2020-09-21 13:34:35 -0600

    Lesson Repo did not clone.  Completed Lesson work here.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+let codeIndex = 0;
+
 function init() {
   // your code here
+  document.addEventListener('keydown', onKeyDownHandler)
+
+  function onKeyDownHandler(e){
+    var key = e.key
+      if(key === codes[codeIndex]){
+        codeIndex = codeIndex + 1;
+        if (codeIndex === codes.length){
+        alert ('Hooray!');
+        codeIndex = 0;
+        }
+      } else {
+        codeIndex = 0;
+      }
+  }
+
 }
+
+init()
